66320587a8eddf388744872480db2ec6595e136b
skmtseiyaku <skmtseiyaku@outlook.com>
2022-03-02 14:14:26 +0900

    Fixed bug & formatted

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
-use std::io::{stdin,stdout,Write};
+use std::io::{stdin, stdout, Write};
 
-fn main(){
-    let mut a=String::new();
-    loop{
+fn main() {
+    let mut a = String::new();
+    loop {
         print!("Input arity: ");
         stdout().flush().unwrap();
-        stdin().read_line(&mut a).expect("Error occurred.");
-        let a:i32=match guess.trim().parse(){
+        stdin().read_line(&mut a).expect("error");
+        let a: u64 = match a.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Eroor occurred.");
+                continue;
+            }
         };
-        
-        let mut i=1;
-        while i<=a {
-            print!("{} ",fbnc(i));
-            i+=1;
+
+        let mut i = 1;
+        while i <= a {
+            print!("{} ", fbnc(i));
+            i += 1;
         }
         println!("");
         break;
     }
 }
 
-fn fbnc(a:i32)->i32{
-    if a==1||a==2{
+fn fbnc(a: u64) -> u64 {
+    if a == 1 || a == 2 {
         return 1;
     }
-    return fbnc(a-2)+fbnc(a-1);
+    return fbnc(a - 2) + fbnc(a - 1);
 }
